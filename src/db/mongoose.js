d7c3b6624588d362dd63634a0803c404d22b6a64
unknown <itayfeiner@gmail.com>
2022-09-12 15:25:38 +0300

    pleaseeee

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,14 +1,12 @@
-const mongoose = require('mongoose')
-//const mongodb = 'mongodb://127.0.0.1:27017/task-manager-api'
-mongoose.connect(process.env.MONGOOSE_URL, {
+const mongoose = require('mongoose');
+ 
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false,
-})
-
-
-// var mongoose = require('mongoose');
-// var mongoDB = 'mongodb://127.0.0.1/my_database';
-// mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}).then(() => {
+    console.log('MongoDB successfully connected. Database URL:', process.env.MONGODB_URL);
+}).catch(error => {
+    console.log('Error establishing MongoDB connection. Error:', error.message);
+});    
